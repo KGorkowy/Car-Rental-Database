6e67cf156c23262a5c50364da7b5fc10a465250a
Kamil <k.gorkowy@op.pl>
2024-03-02 13:11:08 +0100

    Customer validation improved

--- src/main/java/com/example/demo/customers/Customer.java
+++ src/main/java/com/example/demo/customers/Customer.java
@@ -1,7 +1,10 @@
 package com.example.demo.customers;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
 
 @Table
 @Entity
@@ -10,7 +13,11 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "first name is mandatory")
     private String firstName;
+    @NotBlank(message = "Please provide surname")
     private String surname;
-    private String phoneNumber; // int?
+
+    @Pattern(regexp = "^[ ]*\\d{9}[ ]*$", message = "Phone has a wrong format. IT should be eg: 789789789")
+    private String phoneNumber;
 }
--- src/main/java/com/example/demo/customers/CustomerController.java
+++ src/main/java/com/example/demo/customers/CustomerController.java
@@ -1,7 +1,9 @@
 package com.example.demo.customers;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,46 +24,36 @@ public class CustomerController {
         return customerRepository.findAll();
     }
 
-    @PostMapping("/new")
+    @PostMapping("/")
     @Transactional
-    public Customer addCustomer(@RequestBody Customer customer) {
-        // phone number must be unique and its length must be equal to 9
-        if (customer.getPhoneNumber().length() == 9) {
-            List<Customer> list = customerRepository.findAll();
-            boolean flag = true;
-            for (Customer cust : list) {
-                if (cust.getPhoneNumber().equals(customer.getPhoneNumber())) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                return customerRepository.save(customer);
-            } else {
-                log.info("phone number {} already exists", customer.getPhoneNumber());
-                return customer;
+    public Customer addCustomer(@Valid @RequestBody Customer customer) throws BadRequestException {
+        List<Customer> customers = customerRepository.findAll();
+        boolean isPhoneNumberUnique = true;
+        for (Customer cust : customers) {
+            if (cust.getPhoneNumber().equals(customer.getPhoneNumber())) {
+                isPhoneNumberUnique = false;
+                break;
             }
         }
-        else {
-            log.info("invalid phone number");
-            return customer;
+        if (isPhoneNumberUnique) {
+            return customerRepository.save(customer);
         }
+        throw new BadRequestException("phone number " + customer.getPhoneNumber() + " already exists");
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteCustomer (@PathVariable Long id) {
+    public void deleteCustomer(@PathVariable Long id) {
         customerRepository.deleteById(id);
     }
 
     @PutMapping("/edit")
-    public void editCustomer (@RequestBody Customer customer){
+    public void editCustomer(@Valid @RequestBody Customer customer) {
         Optional<Customer> customer1 = customerRepository.findById(customer.getId());
         if (customer1.isPresent()) {
             customer1.get().setFirstName(customer.getFirstName());
             customer1.get().setSurname(customer.getSurname());
             customer1.get().setPhoneNumber(customer.getPhoneNumber());
-        }
-        else log.info("the given id does not exist");
+        } else log.info("the given id does not exist");
     }
 
 
